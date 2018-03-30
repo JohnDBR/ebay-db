69c071c2b4e5c31c3bd69c0622bd114ce03e80bd
John <djbarbosa@uninorte.edu.co>
2018-03-30 10:37:34 -0500

    [feat]: Login system added
    - Model User and Token were created with its migration
    - Token belongs to user an User has many tokens
    - Sessions and Users controller were created
    - Routes for the controller were added
    - Some changes in the gemfile
    - some add in the gitignore
    - an Authentication service was added
    - a custom exception user not found was added
    - Some importants methods for all the controllers were added in the
      application controller
    - Roles for users were added in the application record
    
    Resolves: #NA
    See also: #NA

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, :username, presence: true, uniqueness: true
+  validates :password, :role, :company, :email, :name, presence: true, on: :create
+
+  before_save :format_downcase
+
+  has_many :tokens, dependent: :destroy
+
+  protected 
+  def format_downcase
+    self.name.downcase!
+    self.email.downcase!
+    self.username.downcase!
+  end
+end
