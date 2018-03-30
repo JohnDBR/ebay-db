6d7b48a265edbd9ece2baa692ce5bf83f60ba8d9
John <djbarbosa@uninorte.edu.co>
2018-03-30 12:38:46 -0500

    [feat]: Routes bad arquicheture fixed
    - The routes were bad builded
    
    Resolves: #NA
    See also: #NA

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
-  skip_before_action :get_current_user, only: [:create] 
+  skip_before_action :get_current_user, only: [:index, :show, :create] 
 
   def index 
     render_ok User.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create]
   # get 'users/:id', to: 'users#show' #What the code up do...
+  put 'users', to: 'users#update'
+  delete 'users', to: 'users#destroy'
 
   resources :sessions, only: :create
   delete 'sessions', to: 'sessions#destroy'
-  
+
 end
