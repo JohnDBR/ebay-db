17ddbcda369f54b35caa13a026752b4a318e37f9
John <djbarbosa@uninorte.edu.co>
2018-04-17 22:44:52 -0500

    [fix]: Now we can make auctions
    - The Auctions needs time to check, but for now is working like that.
    - I forgot a callback in the purchase controller
    
    Resolves: #NA
    See also: #NA

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,6 @@
 class PurchasesController < ApplicationController
   before_action :set_purchase, only: [:show, :set_buyer_score, :set_seller_score, :set_was_shipped, :set_was_delivered]
-  before_action :set_product, only: [:create]
+  before_action :set_product, only: [:create, :finish_auction]
 
   def index
     render_ok @current_user.bought_products
@@ -49,7 +49,8 @@ class PurchasesController < ApplicationController
   def create
     if !@product.is_auction 
       if is_the_destiny_mine?
-        if @product.stock >= params[:quantity]
+        # !params[:quantity].nil?
+        if @product.stock >= params[:quantity] 
           purchase = Purchase.new(buyer_id:@current_user.id, seller_id:@product.user.id, product_id:@product.id, quantity:params[:quantity], total_price:(@product.price*params[:quantity]), destiny:@origin)
           @product.update_attribute(:stock, @product.stock - params[:quantity])
           if purchase.save and @product.save
@@ -65,16 +66,19 @@ class PurchasesController < ApplicationController
   end
 
   def finish_auction
-    if is_my_sale? and is_an_auction?
-      last_bid = @product.bids.last
-      if last_bid.empty?
-        purchase = Purchase.new(buyer_id:last_bid.user_id, seller_id:@current_user.id, quantity:@product.stock, total_price:last_bid.bid)
-        @product.update_attribute(:stock, 0)
-        if_save_succeeds(purchase, options) do |object|
-          render json: {purchase: purchase, product: @product}, status: :ok
-        end  
-      else 
-        render json: {authorization: 'is not a bid'}, status: :unprocessable_entity
+    if im_selling?
+      if is_an_auction?
+        pp last_bid = @product.bids
+        if !last_bid.empty?
+          last_bid = last_bid.last
+          purchase = Purchase.new(buyer_id:last_bid.user_id, seller_id:@current_user.id, quantity:@product.stock, total_price:last_bid.bid)
+          @product.update_attribute(:stock, 0)
+          if_save_succeeds(purchase) do |object|
+            render json: {purchase: purchase, product: @product}, status: :ok
+          end  
+        else 
+          render json: {authorization: 'is not a bid'}, status: :unprocessable_entity
+        end
       end
     end
   end
@@ -86,6 +90,10 @@ class PurchasesController < ApplicationController
 
   def set_product
     @product = Product.find params[:product_id]
+  end
+
+  def im_selling?
+    if @product.user_id == @current_user.id then true else permissions_error ; false end
   end
 
   def is_my_sale?
